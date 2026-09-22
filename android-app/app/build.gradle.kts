f7f29d2149efb5ecb3f62f7ae13c87876000fd72
4regodatbe-cpu <4regoda.tbe@gmail.com>
2026-09-22 15:03:43 +0300

    Исправить совместимость компилятора Android

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -16,4 +16,8 @@ android {
     }
 
     buildTypes { release { isMinifyEnabled = false } }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
