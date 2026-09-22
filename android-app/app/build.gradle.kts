78e723de7bc1093e00c9ecfbd9b83c5692df9656
4regodatbe-cpu <4regoda.tbe@gmail.com>
2026-09-22 15:19:44 +0300

    Переименовать приложение в Межгород Calc

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.mezhgorod.calculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes { release { isMinifyEnabled = false } }
