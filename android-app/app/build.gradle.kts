9ce886dfd148de6969d0a0c8cca25d256b9ff151
4regodatbe-cpu <4regoda.tbe@gmail.com>
2026-09-22 15:00:29 +0300

    Выпустить исправленную версию Android 1.0.1

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,4 +1,7 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "ru.mezhgorod.calculator"
@@ -8,8 +11,8 @@ android {
         applicationId = "ru.mezhgorod.calculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes { release { isMinifyEnabled = false } }
